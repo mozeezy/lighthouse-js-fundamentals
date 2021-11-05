8515e91ba4432c8bd83c65b30da655f1456faf09
Mohamed Mohamed <justmoe23@gmail.com>
2021-11-05 13:51:38 +0000

    banana bread assignment

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+while (ingredients <8) {
+  console.log (ingredients)
+}
+
+// Write a for loop that prints out the contents of ingredients:
+
+for (const ingredients of ingredients) {
+  console.log (ingredients)
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+for (var i = 0; i < 1; i++) {
+  while (ingredients <8) {
+    console.log (ingredients.reverse)
+  }
+}
